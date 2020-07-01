fae8227256388ee41e16eb119452a1e928c1c219
Dang Pham <thienvu453@gmail.com>
2020-07-01 11:41:17 +0700

    project init

---
+++ src/main/java/codegym/AppInit.java
@@ -0,0 +1,56 @@
+package codegym;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    /**
+     * Specify {@link Configuration @Configuration}
+     * and/or {@link Component @Component} classes to be
+     * provided to the {@linkplain #createRootApplicationContext() root application context}.
+     *
+     * @return the configuration classes for the root application context, or {@code null}
+     * if creation and registration of a root context is not desired
+     */
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    /**
+     * Specify {@link Configuration @Configuration}
+     * and/or {@link Component @Component} classes to be
+     * provided to the {@linkplain #createServletApplicationContext() dispatcher servlet
+     * application context}.
+     *
+     * @return the configuration classes for the dispatcher servlet application context or
+     * {@code null} if all configuration is specified through root config classes.
+     */
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{};
+    }
+
+    /**
+     * Specify the servlet mapping(s) for the {@code DispatcherServlet} &mdash;
+     * for example {@code "/"}, {@code "/app"}, etc.
+     *
+     * @see #registerDispatcherServlet(ServletContext)
+     */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        return new Filter[] { characterEncodingFilter};
+    }
+}
